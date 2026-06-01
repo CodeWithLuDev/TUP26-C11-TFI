6fe052eb974b15ecc026c226734d5e6c199f80f5
Mara <maravillalba0308@gmail.com>
2026-05-31 21:04:50 -0300

    feat: implemente planteles completos por grupos con dorsales y nombres para el fixture

---
+++ Entregas/mara/src/data/jugadores.js
@@ -0,0 +1,6 @@
+// src/data/jugadores.js
+// Re-exporta todo desde el módulo de jugadores
+// Este archivo existe para compatibilidad con imports anteriores
+
+export { JUGADORES, getPlantelPorEquipo, getJugadoresPartido, buscarJugador }
+  from "./jugadores/index.js";
